b2d7be03e6a2696df4127d34af186dca9a3ae0c7
David Tran <davtran303@gmail.com>
2021-03-25 17:51:16 -0600

    new bootstrap

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,12 +2,19 @@ import React from "react";
 import Card from "./components/Card"
 import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Portfolio from "./pages/portfolio"
 
 function App() {
   return (
     <div>
     <NavBar>
-      <Card/>
+      <About/>
+      <Contact/>
+      <Portfolio>
+        <Card/>
+      </Portfolio>
     </NavBar>
     <Footer/>
     </div>

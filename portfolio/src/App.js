96971ac6becdfb2987705a85165568d0e5888399
David Tran <davtran303@gmail.com>
2021-03-25 18:24:17 -0600

    new routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,21 +4,66 @@ import NavBar from "./components/Navbar";
 import Footer from "./components/Footer";
 import About from "./pages/about";
 import Contact from "./pages/contact";
-import Portfolio from "./pages/portfolio"
+import Portfolio from "./pages/portfolio";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div>
-    <NavBar>
-      <About/>
-      <Contact/>
-      <Portfolio>
-        <Card/>
-      </Portfolio>
-    </NavBar>
-    <Footer/>
+    <Router>
+      <NavBar>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Contact</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Portfolio</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/">
+            <Contact/>
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio>
+              <Card/>
+            </Portfolio>
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+        </NavBar>
+      <Footer/>
+    </Router>
     </div>
   );
+function Contact() {
+  return <h2>Contact</h2>;
+}
+
+function Portfolio() {
+  return <h2>Portfolio</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
 }
 
 export default App;
--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -10,10 +10,10 @@ function NavBar() {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="mr-auto">
-      <Nav.Link href="#home">Contact</Nav.Link>
-      <Nav.Link href="#link">Portfolio</Nav.Link>
-      <Nav.Link href="#home">About</Nav.Link>
-      <Nav.Link href="#link">Link</Nav.Link>
+      <Nav.Link href="#contact">Contact</Nav.Link>
+      <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link href="#link">LinkedIn</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>

cd6192b96e8b4f86c4cfe8bf22d0d0e4269ae364
David Tran <davtran303@gmail.com>
2021-04-21 18:13:40 -0600

    new words

--- src/component/pages/Aboutme/Aboutme.js
+++ src/component/pages/Aboutme/Aboutme.js
@@ -18,7 +18,7 @@ const Aboutme = () => {
             I currently have no experience in the web development industry but I really want to be part of your company’s web development team in order to learn more about complex codes that can be used in everyday life. On my free time, I am currently trying to reclone Facebook and Amazon from scratch while trying to learn Python and C++.
             Learning these new languages and attending the boot camp helped me explore my skills and creativity. As a result, I realized everything that I have tried up to now, that I truly enjoy this career because it allows for endless creativity. I believe this has helped me immensely when it comes to conceptualizing projects because I can think about the end goal and the experiences of the users. 
             <br/>
-            I would be honored to bring my skills, passion, and work ethic to support your company’s team. I am sure that I would work harder than everyone else because although I’m the least experienced in order to help the company goals. Finally, I thank you for taking the time to read my cover letter.
+            I would be honored to bring my skills, passion, and work ethic to support your company’s team. I am sure that I would work harder than everyone else because although I’m the least experienced in order to help the company goals.
             <br/>
             </p1>
                
